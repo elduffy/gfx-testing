73a9653069e8a04c27fbf65a45878969c7ad693c
Eric Duffy <eric.l.duffy@gmail.com>
2025-04-17 15:04:09 -0500

    Use 2.0 tinyobjloader api

--- cmake/fetch-tinyobjloader.cmake
+++ cmake/fetch-tinyobjloader.cmake
@@ -1,7 +1,7 @@
 FetchContent_Declare(
         tinyobjloader
         GIT_REPOSITORY https://github.com/tinyobjloader/tinyobjloader.git
-        GIT_TAG v1.0.7
+        GIT_TAG v2.0.0rc13
         GIT_SHALLOW ON
         GIT_PROGRESS TRUE
         SYSTEM
--- src/obj_loader.cpp
+++ src/obj_loader.cpp
@@ -5,30 +5,31 @@
 #include <SDL3/SDL_log.h>
 
 namespace gfx_testing::model {
-    void loadObjFile(const std::filesystem::path& path) {
+    void loadObjFile(const std::filesystem::path &path) {
         SDL_Log("Loading obj file %s", path.c_str());
 
-        tinyobj::attrib_t attrib;
-        std::vector<tinyobj::shape_t> shapes;
-        std::vector<tinyobj::material_t> materials;
+        tinyobj::ObjReaderConfig reader_config;
+        reader_config.mtl_search_path = path.parent_path();
+        tinyobj::ObjReader reader;
 
-        std::string warn;
-        std::string err;
-        const bool ret = LoadObj(&attrib, &shapes, &materials, &warn, &err, path.c_str(), path.parent_path().c_str());
-
-        if (!err.empty()) {
-            std::cerr << err << std::endl;
-        }
-        if (!warn.empty()) {
-            std::cerr << warn << std::endl;
-        }
-        if (!ret) {
+        if (!reader.ParseFromFile(path.string(), reader_config)) {
+            if (!reader.Error().empty()) {
+                std::cerr << reader.Error() << std::endl;
+            }
             throw std::runtime_error("Failed to load obj file");
         }
+        if (!reader.Warning().empty()) {
+            std::cerr << reader.Warning() << std::endl;
+        }
 
-        SDL_Log("Successfully loaded obj file %s with %zu shapes, %zu materials", path.c_str(), shapes.size(), materials.size());
+        // auto const &attrib = reader.GetAttrib();
+        auto const &shapes = reader.GetShapes();
+        auto const &materials = reader.GetMaterials();
 
-        for (auto const& shape : shapes) {
+        SDL_Log("Successfully loaded obj file %s with %zu shapes, %zu materials", path.c_str(), shapes.size(),
+                materials.size());
+
+        for (auto const &shape: shapes) {
             SDL_Log("Shape %s has %zu indices", shape.name.c_str(), shape.mesh.indices.size());
         }
     }
