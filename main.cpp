a655378369af149607d6fda0677934d74a691cd5
Eric Duffy <eric.l.duffy@gmail.com>
2025-04-18 06:41:50 -0500

    Clear window to red

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <sdl.hpp>
 #include <util.hpp>
 #include <shader_models.hpp>
+#include <boost/scope/scope_exit.hpp>
 
 #include "glm/ext/matrix_clip_space.hpp"
 #include "glm/ext/matrix_transform.hpp"
@@ -39,6 +40,37 @@ void createScene(gfx_testing::sdl::SdlContext const &context, std::filesystem::p
                                                         "content/shaders/src/solid_color.frag.hlsl",
                                                         0,
                                                         0, 0, 0);
+
+    // SDL_GPUGraphicsPipelineCreateInfo pipelineCreateInfo{
+    //
+    // };
+    SDL_GPUCommandBuffer *commandBuffer = SDL_AcquireGPUCommandBuffer(context.mDevice);
+    if (commandBuffer == nullptr) {
+        throw std::runtime_error("Failed to acquire command buffer");
+    }
+    boost::scope::scope_exit submitCommandBufferGuard([commandBuffer] {
+        SDL_SubmitGPUCommandBuffer(commandBuffer);
+    });
+
+    SDL_GPUTexture *swapchainTexture = nullptr;
+    if (!SDL_WaitAndAcquireGPUSwapchainTexture(commandBuffer, context.mWindow, &swapchainTexture, nullptr, nullptr)) {
+        throw std::runtime_error("Failed to acquire swapchain texture");
+    }
+
+    if (swapchainTexture == nullptr) {
+        SDL_Log("Swapchain texture is null");
+        return;
+    }
+
+    const SDL_GPUColorTargetInfo colorTargetInfo{
+        .texture = swapchainTexture,
+        .clear_color = {1.0, 0, 0, 1},
+        .load_op = SDL_GPU_LOADOP_CLEAR,
+        .store_op = SDL_GPU_STOREOP_STORE,
+    };
+    SDL_GPURenderPass *renderPass = SDL_BeginGPURenderPass(commandBuffer, &colorTargetInfo, 1, nullptr);
+    SDL_EndGPURenderPass(renderPass);
+
     auto mvpMatrix = createMVPMatrix(context);
 }
 
