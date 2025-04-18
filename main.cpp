bfa3cca7d48e838840ee2b5cf2f3a5177fc0a8c7
Eric Duffy <eric.l.duffy@gmail.com>
2025-04-18 06:09:04 -0500

    createScene method

--- main.cpp
+++ main.cpp
@@ -8,9 +8,7 @@ void handleEvent(SDL_Event const &event) {
     // SDL_LogWarn(SDL_LOG_CATEGORY_APPLICATION, "Unhandled event type: 0x%x", event.type);
 }
 
-int main() {
-    gfx_testing::sdl::SdlContext context;
-    auto projectRoot = gfx_testing::util::getProjectRoot();
+void createScene(gfx_testing::sdl::SdlContext const &context, std::filesystem::path const &projectRoot) {
     gfx_testing::model::loadObjFile(projectRoot / "content/models/basic-shapes.obj");
     auto vertexShader = gfx_testing::util::loadShader(context,
                                                       projectRoot / "content/shaders/src/pos_color_transform.vert.hlsl",
@@ -21,6 +19,12 @@ int main() {
                                                         "content/shaders/src/solid_color.frag.hlsl",
                                                         0,
                                                         0, 0, 0);
+}
+
+int main() {
+    gfx_testing::sdl::SdlContext context;
+    const auto projectRoot = gfx_testing::util::getProjectRoot();
+    createScene(context, projectRoot);
     gfx_testing::sdl::runEventLoop(handleEvent);
     return 0;
 }
