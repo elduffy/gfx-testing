19077079142ee3de5d2994493285f915c7215e5e
Eric Duffy <eric.l.duffy@gmail.com>
2025-04-20 14:27:44 -0500

    Emit reflection json using spirv-cross

--- content/shaders/build-shaders.sh
+++ content/shaders/build-shaders.sh
@@ -1,27 +1,34 @@
 set -euo pipefail
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+mkdir -p "$SCRIPT_DIR/compiled/spirv"
 
 for filename in "$SCRIPT_DIR"/src/*.vert.hlsl; do
     if [ -f "$filename" ]; then
-        outfile=${filename/.hlsl/.spv}
-        outfile=${outfile/src/compiled\/spirv}
-        shadercross "$filename" -o "$outfile"
+        spv_outfile=${filename/.hlsl/.spv}
+        spv_outfile=${spv_outfile/src/compiled\/spirv}
+        shadercross "$filename" -o "$spv_outfile"
+        json_outfile=${spv_outfile/.spv/.json}
+        spirv-cross "$spv_outfile" --reflect --output "$json_outfile"
     fi
 done
 
 for filename in "$SCRIPT_DIR"/src/*.frag.hlsl; do
     if [ -f "$filename" ]; then
-        outfile=${filename/.hlsl/.spv}
-        outfile=${outfile/src/compiled\/spirv}
-        shadercross "$filename" -o "$outfile"
+        spv_outfile=${filename/.hlsl/.spv}
+        spv_outfile=${spv_outfile/src/compiled\/spirv}
+        shadercross "$filename" -o "$spv_outfile"
+        json_outfile=${spv_outfile/.spv/.json}
+        spirv-cross "$spv_outfile" --reflect --output "$json_outfile"
     fi
 done
 
 for filename in "$SCRIPT_DIR"/src/*.comp.hlsl; do
     if [ -f "$filename" ]; then
-        outfile=${filename/.hlsl/.spv}
-        outfile=${outfile/src/compiled\/spirv}
-        shadercross "$filename" -o "$outfile"
+        spv_outfile=${filename/.hlsl/.spv}
+        spv_outfile=${spv_outfile/src/compiled\/spirv}
+        shadercross "$filename" -o "$spv_outfile"
+        json_outfile=${spv_outfile/.spv/.json}
+        spirv-cross "$spv_outfile" --reflect --output "$json_outfile"
     fi
 done
