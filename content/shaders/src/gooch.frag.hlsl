8e157a0e1738f7e9b112116ccf548b01774a0831
Eric Duffy <eric.l.duffy@gmail.com>
2025-04-25 11:20:55 -0500

    Normalize vectors in shader

--- content/shaders/src/gooch.frag.hlsl
+++ content/shaders/src/gooch.frag.hlsl
@@ -17,15 +17,19 @@ struct Input
 
 float4 main(Input input) : SV_Target0
 {
+    float3 norm = normalize(input.normalCamS);
+    float3 cameraDir = normalize(input.cameraDirCamS);
+    float3 lightDir = normalize(input.lightDirCamS);
     // See Real Time Rendering 5.1
     float3 cool = coolColor + 0.25 * input.color.xyz;
     float3 warm = warmColor + 0.25 * input.color.xyz;
-    float diff = dot(input.normalCamS, input.lightDirCamS);
+    float diff = dot(norm, lightDir);
     float t = (diff + 1.0) / 2.0;
-    float3 r = 2.0 * diff * input.normalCamS - input.lightDirCamS;
-    float s = saturate(100.0 * dot(r, input.cameraDirCamS) - 97.0);
+    float3 r = 2.0 * diff * norm - lightDir;
+    float s = saturate(100.0 * dot(r, cameraDir) - 97.0);
 
     float3 highlight = float3(1.0, 1.0, 1.0);
     float3 result = s * highlight + (1.0-s)*(t*warm + (1.0-t)*cool);
     return float4(result, 1.0);
+    //return float4(norm, 1);
 }
