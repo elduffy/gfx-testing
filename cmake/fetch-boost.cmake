ed09f78d6252e9122ef2c36bfcdc06b9dca97625
Eric Duffy <eric.l.duffy@gmail.com>
2025-05-25 10:19:52 -0500

    Use submodule for boost

--- cmake/fetch-boost.cmake
+++
@@ -1,9 +0,0 @@
-FetchContent_Declare(
-        Boost
-        GIT_REPOSITORY https://github.com/boostorg/boost.git
-        GIT_TAG boost-1.88.0
-        GIT_SHALLOW TRUE
-        GIT_PROGRESS TRUE
-        SYSTEM
-)
-FetchContent_MakeAvailable(Boost)
