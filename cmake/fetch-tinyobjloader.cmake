f69074e30509578588afb1a851601328247652dc
Eric Duffy <eric.l.duffy@gmail.com>
2026-01-13 06:04:40 -0600

    timyobjloader vcpkg

--- cmake/fetch-tinyobjloader.cmake
+++
@@ -1,9 +0,0 @@
-FetchContent_Declare(
-        tinyobjloader
-        GIT_REPOSITORY https://github.com/tinyobjloader/tinyobjloader.git
-        GIT_TAG v2.0.0rc13
-        GIT_SHALLOW ON
-        GIT_PROGRESS TRUE
-        SYSTEM
-)
-FetchContent_MakeAvailable(tinyobjloader)
