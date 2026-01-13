03ffd2607f395f9edd4440b0a21272002e526389
Eric Duffy <eric.l.duffy@gmail.com>
2026-01-13 06:28:51 -0600

    clipp vcpkg

--- cmake/fetch-clipp.cmake
+++
@@ -1,9 +0,0 @@
-FetchContent_Declare(
-        clipp
-        GIT_REPOSITORY https://github.com/muellan/clipp.git
-        GIT_TAG 2c32b2f
-        GIT_SHALLOW TRUE
-        GIT_PROGRESS TRUE
-        SYSTEM
-)
-FetchContent_MakeAvailable(clipp)
