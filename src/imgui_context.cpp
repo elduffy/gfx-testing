80e681b33759065da82ad5bdf31bf81bb245cf54
Eric Duffy <eric.l.duffy@gmail.com>
2026-02-05 08:23:33 -0600

    ECS UI show parent ID and components as tree

--- src/imgui_context.cpp
+++ src/imgui_context.cpp
@@ -83,30 +83,45 @@ namespace gfx_testing::imgui {
                 static constexpr ImGuiTableFlags tableFlags =
                         ImGuiTableFlags_BordersV | ImGuiTableFlags_BordersOuterH | ImGuiTableFlags_Resizable |
                         ImGuiTableFlags_RowBg | ImGuiTableFlags_NoBordersInBody | ImGuiTableFlags_SizingStretchProp;
-                if (ImGui::BeginTable("ecs", 3, tableFlags)) {
-                    static constexpr ImGuiTableColumnFlags defaultColumnFlags = ImGuiTableColumnFlags_NoHide;
+                if (ImGui::BeginTable("ecs", 4, tableFlags)) {
+                    static constexpr ImGuiTableColumnFlags defaultColumnFlags =
+                            ImGuiTableColumnFlags_NoHide | ImGuiTableColumnFlags_WidthStretch;
                     ImGui::TableSetupColumn("ID", defaultColumnFlags);
                     ImGui::TableSetupColumn("Name", defaultColumnFlags);
-                    ImGui::TableSetupColumn("RenderObject", defaultColumnFlags);
+                    ImGui::TableSetupColumn("Parent ID", defaultColumnFlags);
+                    ImGui::TableSetupColumn("Components", defaultColumnFlags);
                     ImGui::TableHeadersRow();
 
                     ecs.mRegistry.sort<entt::entity>([](entt::entity const &lhs, entt::entity const &rhs) {
                         return ecs::EntityId::getId(lhs) < ecs::EntityId::getId(rhs);
                     });
                     for (auto const entity: ecs.mRegistry.view<entt::entity>()) {
+                        auto const entityId = ecs.get(entity);
+                        ImGui::PushID(entityId.getId());
                         ImGui::TableNextRow();
                         ImGui::TableNextColumn();
-                        ImGui::Text("%u", ecs::EntityId::getId(entity));
+                        ImGui::Text("%u", entityId.getId());
                         ImGui::TableNextColumn();
-                        ImGui::Text("%s", ecs.get(entity).getName());
+                        ImGui::Text("%s", entityId.getName());
                         ImGui::TableNextColumn();
-                        if (auto const *renderObject = ecs.mRegistry.try_get<render::RenderObject>(entity);
-                            renderObject == nullptr) {
-                            ImGui::TextDisabled("--");
+                        if (auto const parentOpt = entityId.getParent(); parentOpt.has_value()) {
+                            ImGui::Text("%u", parentOpt->getId());
                         } else {
-                            ImGui::Value("Pipeline", getName(renderObject->getPipelineName()));
-                            ImGui::Value("Position", renderObject->getPositionWs());
+                            ImGui::TextDisabled("--");
                         }
+
+                        ImGui::TableNextColumn();
+                        if (ImGui::TreeNode("RenderObject")) {
+                            if (auto const *renderObject = ecs.mRegistry.try_get<render::RenderObject>(entity);
+                                renderObject == nullptr) {
+                                ImGui::TextDisabled("--");
+                            } else {
+                                ImGui::Value("Pipeline", getName(renderObject->getPipelineName()));
+                                ImGui::Value("Position", renderObject->getPositionWs());
+                            }
+                            ImGui::TreePop();
+                        }
+                        ImGui::PopID();
                     }
 
                     ImGui::EndTable();
